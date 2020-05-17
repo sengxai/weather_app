b476217e7b8b1801943122ee70c67e3710b698bc
Seng Xai Yang <yangsx7908@uwec.edu>
2020-05-17 16:27:08 -0500

    test

---
+++ precache-manifest.3293756c155e1498dc9329baad007380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9e4c652cbe1d44d7ec602faa220cff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9294c487a896217d1b1d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "612cae5eac39bb380480",
+    "url": "/static/js/2.ce190899.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce190899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9294c487a896217d1b1d",
+    "url": "/static/js/main.d8cb8059.chunk.js"
+  },
+  {
+    "revision": "30259e7a30d4e8b4ab16",
+    "url": "/static/js/runtime-main.a9559881.js"
+  }
+]);
